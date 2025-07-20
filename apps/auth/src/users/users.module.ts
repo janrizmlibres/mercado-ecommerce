0dc5cccce9473efcfe064c96f1a8345afa95df60
Janriz Libres <libres.janriz@gmail.com>
2025-07-20 08:45:18 +0800

    import logger and config modules to users module

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { prismaService } from './prisma.service';
+import { LoggerModule, ConfigModule } from '@app/common';
 
 @Module({
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      envFilePath: './apps/auth/.env.local',
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService, prismaService],
 })
