d38b9a5aa21e9bde8a7465261b07c83c0a0d7062
Janriz Libres <libres.janriz@gmail.com>
2025-07-20 10:37:11 +0800

    move logger and config module from users to auth

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
+import { LoggerModule, ConfigModule } from '@app/common';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      envFilePath: './apps/auth/.env.local',
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -2,15 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from './prisma.service';
-import { LoggerModule, ConfigModule } from '@app/common';
 
 @Module({
-  imports: [
-    LoggerModule,
-    ConfigModule.forRoot({
-      envFilePath: './apps/auth/.env.local',
-    }),
-  ],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
 })
