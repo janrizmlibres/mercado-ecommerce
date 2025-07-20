ef943db5db4819462c0d15a60dd9862292bd0c8c
Janriz Libres <libres.janriz@gmail.com>
2025-07-20 08:42:22 +0800

    add prisma service to auth

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'apps/orders/src/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   async create(createUserDto: CreateUserDto) {}
 }
