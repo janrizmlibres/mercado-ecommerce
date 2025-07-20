25b4f17116a831fe4eed17b70f020fa77b5017f9
Janriz Libres <libres.janriz@gmail.com>
2025-07-20 12:39:46 +0800

    replace process.env with ConfigService

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
 import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 void bootstrap();
