0a324e5d91789d5f6b602ab4dc702f8eaad5d0e7
Janriz Libres <libres.janriz@gmail.com>
2025-07-20 09:36:01 +0800

    implement users service

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from './prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.user.findUniqueOrThrow({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
 }
