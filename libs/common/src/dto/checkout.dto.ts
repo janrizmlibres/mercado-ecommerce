3bc9078b380cbb8f19a80afe5bd427b8be560cd0
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 17:46:09 +0800

    add common checkout dto

---
+++ libs/common/src/dto/checkout.dto.ts
@@ -0,0 +1,4 @@
+export class CheckoutDto {
+  checkoutId: string;
+  redirectUrl: string;
+}
--- libs/common/src/dto/index.ts
+++ libs/common/src/dto/index.ts
@@ -1,2 +1,3 @@
 export * from './user.dto';
 export * from './order-item.dto';
+export * from './checkout.dto';
