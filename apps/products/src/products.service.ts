212222d6ef08b30eb26564435ab6906e5c2e9d70
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 11:53:08 +0800

    implement crud operations in products

--- apps/products/src/products.service.ts
+++ apps/products/src/products.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createProductDto: CreateProductDto) {
+    return this.prismaService.product.create({
+      data: createProductDto,
+    });
+  }
+
+  findAll() {
+    return this.prismaService.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.product.findUniqueOrThrow({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.prismaService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.product.delete({
+      where: { id },
+    });
   }
 }
