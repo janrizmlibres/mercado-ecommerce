d4eec9f18a36c9006cf772a4b35bfd35a38c86ef
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 18:56:20 +0800

    setup notifications module

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { ConfigModule, LoggerModule } from '@app/common';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot(
+      'apps/notifications/.env',
+      Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    ),
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
