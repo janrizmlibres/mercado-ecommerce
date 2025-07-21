f0fae3f678a066ce0c4080f9274195bf2c026d31
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 19:36:51 +0800

    intial logic for sending emails in notifications service

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { NotifyEmailDto } from './dto/notify-email.dto';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NotificationsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly transporter: nodemailer.Transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.configService.get('SMTP_USER'),
+        clientId: this.configService.get('GOOGLE_OAUTH_CLIENT_ID'),
+        clientSecret: this.configService.get('GOOGLE_OAUTH_CLIENT_SECRET'),
+        refreshToken: this.configService.get('GOOGLE_OAUTH_REFRESH_TOKEN'),
+      },
+    });
+  }
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('SMTP_USER'),
+      to: email,
+      subject: 'Mercado Notification',
+      text: 'Test text',
+    });
   }
 }
