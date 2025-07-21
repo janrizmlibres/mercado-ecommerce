fd12cb127722243a54931966ff767f3ed6a34ba6
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 11:18:18 +0800

    configure logger and config modules in products

--- apps/products/src/products.module.ts
+++ apps/products/src/products.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ConfigModule, LoggerModule } from '@app/common';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot(
+      'apps/products/.env.local',
+      Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    ),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
