6f0fd2e8496f559469282a336d2ef805a8479c82
Janriz Libres <libres.janriz@gmail.com>
2025-07-21 11:52:57 +0800

    add validation pipe to products service

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ProductsModule } from './products.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') ?? 3004);
