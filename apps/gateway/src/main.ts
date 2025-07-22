9817e8e9e179fb7f76dcb8614dc0baf4c6d06be9
Janriz Libres <libres.janriz@gmail.com>
2025-07-22 20:40:00 +0800

    change get to getOrThrow

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
-  await app.listen(configService.get<number>('PORT')!);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 void bootstrap();
