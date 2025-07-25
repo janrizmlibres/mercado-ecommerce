46683e364455b4cfd47dfd1923037af18065552e
Janriz Libres <libres.janriz@gmail.com>
2025-07-25 22:37:09 +0800

    add graphql validations in create order dto

--- apps/orders/src/dto/create-order.dto.ts
+++ apps/orders/src/dto/create-order.dto.ts
@@ -2,16 +2,22 @@ import { IsArray, IsEnum, IsNumber, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { OrderItemDto } from '@app/common';
 import { Status } from '.prisma/client';
+import { Field, InputType } from '@nestjs/graphql';
+import { OrderItemModel } from '../models/order-item.model';
 
+@InputType()
 export class CreateOrderDto {
   @IsEnum(Status)
+  @Field()
   status: Status;
 
   @IsNumber()
+  @Field()
   totalPrice: number;
 
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => OrderItemDto)
+  @Field(() => [OrderItemModel])
   orderItems: OrderItemDto[];
 }
