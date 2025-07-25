b32c1bd9e1fc3ccf4031ef1f6b0f80d0d509de0c
Janriz Libres <libres.janriz@gmail.com>
2025-07-25 22:36:42 +0800

    setup federation in gateway module

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, LoggerModule } from '@app/common';
 import * as Joi from 'joi';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { ConfigService } from '@nestjs/config';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
   imports: [
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'orders',
+                url: configService.getOrThrow('ORDERS_GRAPHQL_URL'),
+              },
+            ],
+          }),
+        },
+      }),
+      inject: [ConfigService],
+    }),
     LoggerModule,
     ConfigModule.forRoot(
       './apps/gateway/.env',
