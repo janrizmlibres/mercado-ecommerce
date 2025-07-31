1e929a965a38a29098585e9326ff5a056919c158
Janriz Libres <libres.janriz@gmail.com>
2025-07-31 20:16:59 +0800

    add cart main entry file

---
+++ apps/cart/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { CartModule } from './cart.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(CartModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT')!);
+}
+void bootstrap();
