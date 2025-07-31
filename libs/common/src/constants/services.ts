ed94daf412b9faa2138bbdc79f02ac8e5f37297e
Janriz Libres <libres.janriz@gmail.com>
2025-07-31 20:44:45 +0800

    add orders service in constants

--- libs/common/src/constants/services.ts
+++ libs/common/src/constants/services.ts
@@ -1,4 +1,5 @@
 export const AUTH_SERVICE = 'auth';
 export const PAYMENTS_SERVICE = 'payments';
 export const NOTIFICATIONS_SERVICE = 'notifications';
+export const ORDERS_SERVICE = 'orders';
 export const CACHE_INSTANCE = 'redis';
