b6d04422a319c73da50d4ce26a6bcc8751d55d0d
Janriz Libres <libres.janriz@gmail.com>
2025-07-31 21:04:04 +0800

    add tcp port for orders service

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -35,7 +35,8 @@ import * as Joi from 'joi';
       Joi.object({
         DATABASE_URL: Joi.string().required(),
         REDIS_URL: Joi.string().required(),
-        PORT: Joi.number().required(),
+        HTTP_PORT: Joi.number().required(),
+        TCP_PORT: Joi.number().required(),
         AUTH_HOST: Joi.string().required(),
         AUTH_PORT: Joi.number().required(),
         PAYMENTS_HOST: Joi.string().required(),
