286064e3417b7fb8632c77b6317a7c22af938724
Janriz Libres <libres.janriz@gmail.com>
2025-08-01 17:15:17 +0800

    setup static files in uploads

--- apps/uploads/src/uploads.module.ts
+++ apps/uploads/src/uploads.module.ts
@@ -3,10 +3,16 @@ import { ConfigModule, LoggerModule } from '@app/common';
 import * as Joi from 'joi';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     LoggerModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'images'),
+      serveRoot: '/images',
+    }),
     ConfigModule.forRoot(
       'apps/uploads/.env',
       Joi.object({
