6fbeb5f4c039622e44a05279d62c547a4ac0f7ac
Janriz Libres <libres.janriz@gmail.com>
2025-08-01 17:14:43 +0800

    remove orders service

--- libs/common/src/constants/services.ts
+++ libs/common/src/constants/services.ts
@@ -1,5 +1,4 @@
 export const AUTH_SERVICE = 'auth';
 export const PAYMENTS_SERVICE = 'payments';
 export const NOTIFICATIONS_SERVICE = 'notifications';
-export const ORDERS_SERVICE = 'orders';
 export const CACHE_INSTANCE = 'redis';
